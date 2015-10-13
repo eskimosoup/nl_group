9dc8438d051024b6502ada1de7014a8b57d2e409
Optimised server <support@eskimosoup.co.uk>
2015-10-13 14:46:16 +0100

    More styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def social_media_link(network)
+    return unless global_site_settings[network].present?
+    link_to global_site_settings[network], title: network do
+      image_tag "components/social/#{network.downcase.gsub(' ', '-')}.png", alt: network
+    end
+  end
 end
