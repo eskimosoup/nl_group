8ae3d6eaba569d9c77c93270d651b567444e9ec5
Optimised server <support@eskimosoup.co.uk>
2015-10-15 11:42:29 +0100

    Amend FAQs

--- app/presenters/frequently_asked_question_presenter.rb
+++ app/presenters/frequently_asked_question_presenter.rb
@@ -6,7 +6,13 @@ class FrequentlyAskedQuestionPresenter < BasePresenter
   end
 
   def toggle_question
-    h.link_to frequently_asked_question.question, '#'
+    h.link_to frequently_asked_question.question, '#', class: 'toggle-class', data: { container: "#faq-#{frequently_asked_question.id}", class: 'hide', return: 'false' }
+  end
+
+  def toggle_answer
+    h.content_tag :div, id: "#faq-#{frequently_asked_question.id}", class: 'hide' do
+      answer
+    end
   end
 
   def answer
