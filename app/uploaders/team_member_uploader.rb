6591cdf2f83068c66d6d14d4f2f0be4762721206
Optimised server <support@eskimosoup.co.uk>
2015-10-15 15:06:08 +0100

    Team members, improve later

--- app/uploaders/team_member_uploader.rb
+++ app/uploaders/team_member_uploader.rb
@@ -1,11 +1,11 @@
 class TeamMemberUploader < Optimadmin::ImageUploader
 
   version :index do
-    process resize_to_fill: [250, 250]
+    process resize_to_fill: [96, 96]
   end
 
   version :show do
-    process resize_to_fill: [500, 500]
+    process resize_to_fill: [250, 250]
   end
 
 end
