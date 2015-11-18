3e18358e60a816faab234743655261c661a84b66
James Doyley <jdoyley@gmail.com>
2015-11-18 13:20:03 +0000

    Add basic mailer styling and add in linked in logo and link

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   layout 'mailer'
   default from: Proc.new{ ApplicationMailer.site_email }
+  before_action :global_site_settings
 
   def self.site_email
     @site_email = begin
@@ -22,5 +23,9 @@ class ApplicationMailer < ActionMailer::Base
         "Optimised"
       end
     end
+  end
+
+  def global_site_settings
+    @global_site_settings ||= Optimadmin::SiteSetting.current_environment
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { host: "nlgroup.ludo5.co.uk" }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
