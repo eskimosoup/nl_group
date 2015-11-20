4cb6e3d6617d082101f61001fda908066e3176f7
Optimised server <support@eskimosoup.co.uk>
2015-11-20 15:09:11 +0000

    Setup navigation

--- app/presenters/member_profile_presenter.rb
+++ app/presenters/member_profile_presenter.rb
@@ -15,61 +15,61 @@ class MemberProfilePresenter < BasePresenter
 
   def basic_information_link
     if basic_information?
-      h.link_to "About You", h.edit_member_area_basic_information_path
+      h.link_to "About You", h.edit_member_area_basic_information_path, class: active_class(h.edit_member_area_basic_information_path)
     else
-      h.link_to "About You", h.new_member_area_basic_information_path
+      h.link_to "About You", h.new_member_area_basic_information_path, class: active_class(h.new_member_area_basic_information_path)
     end
   end
 
   def work_eligibility_link
     if work_eligibility?
-      h.link_to "Eligibility To Work", h.edit_member_area_work_eligibility_path
+      h.link_to "Eligibility To Work", h.edit_member_area_work_eligibility_path, class: active_class(h.new_member_area_work_eligibility_path)
     else
-      h.link_to "Eligibility To Work", h.new_member_area_work_eligibility_path
+      h.link_to "Eligibility To Work", h.new_member_area_work_eligibility_path, class: active_class(h.new_member_area_work_eligibility_path)
     end
   end
 
   def addresses_link
-    h.link_to "Address History", h.member_area_member_addresses_path
+    h.link_to "Address History", h.member_area_member_addresses_path, class: active_class(h.member_area_member_addresses_path)
   end
 
   def member_qualification_link
     if member_qualification?
-      h.link_to "Qualifications", h.edit_member_area_member_qualification_path
+      h.link_to "Qualifications", h.edit_member_area_member_qualification_path, class: active_class(h.edit_member_area_member_qualification_path)
     else
-      h.link_to "Qualifications", h.new_member_area_member_qualification_path
+      h.link_to "Qualifications", h.new_member_area_member_qualification_path, class: active_class(h.new_member_area_member_qualification_path)
     end
   end
 
   def member_training_link
     if member_training?
-      h.link_to "Training", h.edit_member_area_member_training_path
+      h.link_to "Training", h.edit_member_area_member_training_path, class: active_class(h.edit_member_area_member_training_path)
     else
-      h.link_to "Training", h.new_member_area_member_training_path
+      h.link_to "Training", h.new_member_area_member_training_path, class: active_class(h.new_member_area_member_training_path)
     end
   end
 
   def referee_and_emergency_contact_link
-    h.link_to "Referees and Emergency Contacts", h.member_area_referees_and_emergency_contact_path
+    h.link_to "Referees and Emergency Contacts", h.member_area_referees_and_emergency_contact_path, class: active_class(h.member_area_referees_and_emergency_contact_path)
   end
 
   def occupational_health_screening_link
-    h.link_to "Occupational Health Screening", h.member_area_occupational_health_screening_path
+    h.link_to "Occupational Health Screening", h.member_area_occupational_health_screening_path, class: active_class(h.member_area_occupational_health_screening_path)
   end
 
   def dbs_check_link
     if dbs_check?
-      h.link_to "DBS Check", h.edit_member_area_dbs_check_path
+      h.link_to "DBS Check", h.edit_member_area_dbs_check_path, class: active_class(h.edit_member_area_dbs_check_path)
     else
-      h.link_to "DBS Check", h.new_member_area_dbs_check_path
+      h.link_to "DBS Check", h.new_member_area_dbs_check_path, class: active_class(h.new_member_area_dbs_check_path)
     end
   end
 
   def payment_information_link
     if payment_information?
-      h.link_to "Payment Information", h.edit_member_area_payment_information_path
+      h.link_to "Payment Information", h.edit_member_area_payment_information_path, class: active_class(h.edit_member_area_payment_information_path)
     else
-      h.link_to "Payment Information", h.new_member_area_payment_information_path
+      h.link_to "Payment Information", h.new_member_area_payment_information_path, class: active_class(h.new_member_area_payment_information_path)
     end
   end
 
@@ -156,5 +156,11 @@ class MemberProfilePresenter < BasePresenter
 
   def emergency_contact?
     member_profile.emergency_contact.present?
+  end
+
+  private
+
+  def active_class(route)
+    h.current_page?(route) ? 'active' : ''
   end
 end
