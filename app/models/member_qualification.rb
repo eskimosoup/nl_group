416f886dff188058d046b20c37e72ccf96d6c41c
James Doyley <jdoyley@gmail.com>
2015-11-20 12:18:22 +0000

    Add error messages on member qualifications and immunisation histories

--- app/models/member_qualification.rb
+++ app/models/member_qualification.rb
@@ -4,7 +4,7 @@ class MemberQualification < ActiveRecord::Base
   accepts_nested_attributes_for :qualifications, allow_destroy: true
 
   validates :member_profile, presence: true
-  validates :afc_band, numericality: { only_integer: true }, inclusion: { in: (2..7).to_a }, allow_nil: true
+  validates :afc_band, numericality: { only_integer: true, allow_blank: true }, inclusion: { in: (2..7).to_a }, allow_nil: true
   validates :registration_number, presence: true, if: Proc.new{|z| z.register.present? }
   validates :expiry_date, presence: true, if: Proc.new{|x| x.register.present? }
 end
