6cda6b78b17f22364e86677473a19bfaaca27984
James Doyley <jdoyley@gmail.com>
2015-11-25 15:42:31 +0000

    Update admin registration completed mailer

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -8,10 +8,10 @@ class AdminMailer < ApplicationMailer
   def registration_complete(member_profile)
     @member_profile = member_profile
     @registration_data = RegistrationData.new(@member_profile)
-    attachments["#{ @registration_data.member_full_name }.pdf"] = {
+    attachments["#{ @member_profile.full_name }.pdf"] = {
       mime_type: "application/pdf",
       content: WickedPdf.new.pdf_from_string(render_to_string(pdf: "registration", template: 'member_area/base/registration_data.html.erb'))
     }
-    mail to: "george@optimised.today", subject: "Email"
+    mail to: "george@optimised.today", subject: "#{ @member_profile.full_name } has completed their registration form"
   end
 end
