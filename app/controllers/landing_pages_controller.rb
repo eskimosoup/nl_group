05613e4a49378eaec651106f35696d47b181d6bf
James Doyley <jdoyley@gmail.com>
2015-11-30 12:59:39 +0000

    Add landing page facade

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -1,13 +1,10 @@
 class LandingPagesController < ApplicationController
 
   def show
-    @landing_page = LandingPage.friendly.find(params[:id])
-    if @landing_page.slug != params[:id]
-      redirect_to landing_page_path(@landing_page)
+    landing_page = LandingPage.friendly.find(params[:id])
+    if landing_page.slug != params[:id]
+      redirect_to landing_page_path(landing_page)
     end
-    @landing_page_contents = @landing_page.landing_page_contents
-    @landing_page_why_works = @landing_page.landing_page_why_works.displayed.order(position: :asc)
-    @testimonials = @landing_page.testimonials
-    @frequently_asked_questions = @landing_page.frequently_asked_questions
+    @landing_page_facade = LandingPageFacade.new(landing_page)
   end
 end
