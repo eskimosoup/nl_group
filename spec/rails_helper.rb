669ec65c304331b23f141ea1ea4938474934c0f9
Optimised server <support@eskimosoup.co.uk>
2016-01-04 11:27:55 +0000

    Team member tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,11 +66,11 @@ RSpec.configure do |config|
       next if klass.anonymous?
       klass.class_eval do
         def cache_dir
-          "#{Rails.root}/spec/support/uploads/tmp"
+          "#{Rails.root}/public/spec/support/uploads/tmp"
         end
 
         def store_dir
-          "#{Rails.root}/spec/support/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+          "#{Rails.root}/public/spec/support/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
         end
       end
     end
