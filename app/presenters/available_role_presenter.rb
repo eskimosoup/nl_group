3a916e591f0bd0e146471b68e61ed7ceca887690
Paul <pauloptimised@users.noreply.github.com>
2016-03-18 16:43:58 +0000

    Fixes

--- app/presenters/available_role_presenter.rb
+++ app/presenters/available_role_presenter.rb
@@ -1,4 +1,4 @@
 class AvailableRolePresenter < BasePresenter
   presents :available_role
-  delegate :title, :rates_of_pay, to: :available_role
+  delegate :title, :rates_of_pay, :workable_link, to: :available_role
 end
