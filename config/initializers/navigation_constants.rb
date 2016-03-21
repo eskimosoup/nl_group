e099d0b1512930cd380ab6222b3aa91fb0ecd493
Paul <pauloptimised@users.noreply.github.com>
2016-03-21 16:19:53 +0000

    Amend navigation

--- config/initializers/navigation_constants.rb
+++ config/initializers/navigation_constants.rb
@@ -1,7 +1,8 @@
-NavigationMenus = %w( header footer )
+NavigationMenus = %w( header footer ).freeze
 
 NavigationLinks = {
+  'Page' => 'Page',
   'Module Page' => 'Optimadmin::ModulePage',
   'External Link' => 'Optimadmin::ExternalLink',
   'Additional Content' => 'AdditionalRow'
-}
+}.freeze
